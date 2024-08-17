a85a20a2622010ef6b37777dc6ad7a2bf9638d17
adityapandey14 <anshu61796@gmail.com>
2024-08-17 09:28:55 +0530

    4 screens has been added

---
+++ app/src/main/java/com/example/foodordering/StartActivity.kt
@@ -0,0 +1,32 @@
+package com.example.foodordering
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.foodordering.databinding.ActivityStartBinding
+
+class StartActivity : AppCompatActivity() {
+
+    //To connect StartActivity page with loginActivity page
+    private val binding: ActivityStartBinding by lazy {
+        ActivityStartBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)  // Set the content view to the root of the binding
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding.nextButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
